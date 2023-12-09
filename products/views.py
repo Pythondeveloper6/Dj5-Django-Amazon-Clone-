8b5e28bbe225be139d2e6de29f8a701e350d6fd1
Mahmoud Ahmed <pythondeveloper6@gmail.com>
2023-12-09 21:46:11 +0200

    show product reviews from view

--- products/views.py
+++ products/views.py
@@ -6,11 +6,24 @@ from .models import Product , Brand , Review
 
 
 
+
+# queryset : [products] : filter
+# context : user
+
 class ProductList(ListView):
     model = Product
     
     
     
+# context{} , queryset : Product.objects.all() : 1 : option   2 : method : overide
+# querset : main query : detail product       
+# context : extra data  : reviews , images
     
-class ProductDetail(DetailView):
+class ProductDetail(DetailView):    
     model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) # dict : object product
+        context["reviews"] = Review.objects.filter(product=self.get_object())
+        return context
+        
