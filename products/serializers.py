48a0cc61edb10d1a54b74bd0f684f763eae327da
Mahmoud Ahmed <pythondeveloper6@gmail.com>
2023-12-16 21:30:58 +0200

    show brand name in products

--- products/serializers.py
+++ products/serializers.py
@@ -4,6 +4,7 @@ from .models import Product , Brand
 
 
 class ProductListSerializer(serializers.ModelSerializer):
+    brand = serializers.StringRelatedField()
     class Meta:
         model = Product
         fields = '__all__'
