f1aeb6dbc6bf95d2f3538c860a6bed4379987b5c
Mahmoud Ahmed <pythondeveloper6@gmail.com>
2023-12-27 21:50:52 +0200

    design settings app models

--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Settings(models.Model):
+    name = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='settings')
+    subtitle = models.TextField(max_length=500)
+    call_us = models.CharField(max_length=25)
+    email_us = models.CharField(max_length=25)
+    emails = models.TextField(max_length=50)
+    phones = models.TextField(max_length=50)
+    address = models.TextField(max_length=100)
+    android_app = models.URLField(null=True,blank=True)
+    ios_app = models.URLField(null=True,blank=True)
+    facebook = models.URLField(null=True,blank=True)
+    youtube = models.URLField(null=True,blank=True)
+    
+    def __str__(self):
+        return self.name
