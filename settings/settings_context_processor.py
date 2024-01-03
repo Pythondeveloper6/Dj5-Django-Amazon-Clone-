3b64508f2caf4a781cbcdfbee3b4949f10e22c57
Mahmoud Ahmed <pythondeveloper6@gmail.com>
2024-01-03 21:38:34 +0200

    add home data

--- settings/settings_context_processor.py
+++ settings/settings_context_processor.py
@@ -5,13 +5,14 @@ from django.core.cache import cache
 
 def get_settings(request):
 
-    # check data in cache 
-    try:
-        settings_data = cache.get('settings_data')
-        print('new cache')
-    except Exception:
-        print('new data')
-        settings_data = Settings.objects.last()
-        cache.set('settings_data',settings_data,60*60*24*30)
+    # # check data in cache 
+    # try:
+    #     settings_data = cache.get('settings_data')
+    #     print('new cache')
+    # except Exception:
+    #     print('new data')
+    #     settings_data = Settings.objects.last()
+    #     cache.set('settings_data',settings_data,60*60*24*30)
     
+    settings_data = Settings.objects.last()
     return {'settings_data':settings_data}
--- settings/views.py
+++ settings/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.db.models import Count
+from products.models import Product , Brand , Review
 
 def home(request):
+    new_products = Product.objects.filter(flag='New')[:10]
+    sale_products = Product.objects.filter(flag='Sale')[:10]
+    feature_products = Product.objects.filter(flag='Feature')[:6]
     
-    return render(request,'settings/home.html',{})
+    brands = Brand.objects.annotate(product_count=Count('product_brand'))[:10]
+    reviews = Review.objects.all()
+    
+    return render(request,'settings/home.html',{
+        'new_products':new_products , 
+        'sale_products': sale_products , 
+        'feature_products': feature_products , 
+        'brands': brands , 
+        'reviews': reviews
+    })
