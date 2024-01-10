c00b63d59990cfc0008f26adde77e1e45b12ee87
Mahmoud Ahmed <pythondeveloper6@gmail.com>
2024-01-10 22:31:19 +0200

    add to cart

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path 
-from .views import order_list , checkout
+from .views import order_list , checkout , add_to_cart
 
 
 
 urlpatterns = [
     path('' , order_list),
     path('checkout/' , checkout),
-    
+    path('add-to-cart',add_to_cart),
     
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Order , OrderDetail , Cart , CartDetail , Coupon
-
+from products.models import Product 
 
 def order_list(request):
     data = Order.objects.filter(user=request.user)
@@ -11,3 +11,23 @@ def order_list(request):
 def checkout(request):
     
     return render(request,'orders/checkout.html',{})
+
+
+
+
+def add_to_cart(request):
+    product = Product.objects.get(id=request.POST['product_id'])
+    quantity = int(request.POST['quantity'])
+    
+    cart = Cart.objects.get(user=request.user , status='Inprogress')
+    cart_detail , created = CartDetail.objects.get_or_create(cart=cart,product=product)
+    
+    # if not created:
+    #     cart_detail.quantity = cart_detail.quantity + quantity
+    
+    cart_detail.quantity = quantity
+    cart_detail.total = round(product.price * cart_detail.quantity,2)
+    cart_detail.save()
+    
+    return redirect(f'/products/{product.slug}')
+    
