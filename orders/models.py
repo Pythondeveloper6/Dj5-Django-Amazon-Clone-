2d946787069d188d01ca7cafdc011f9a3d701e56
Mahmoud Ahmed <pythondeveloper6@gmail.com>
2024-01-10 22:18:26 +0200

    calculate cart totla

--- orders/models.py
+++ orders/models.py
@@ -48,6 +48,13 @@ class Cart(models.Model):
     coupon = models.ForeignKey('Coupon',related_name='cart_coupon',on_delete=models.SET_NULL,null=True,blank=True)
     total_with_coupon = models.FloatField(null=True,blank=True)
     
+    @property
+    def cart_total(self):
+        total = 0
+        for item in self.cart_detail.all():
+            total += item.total
+        return round(total,2)
+    
 
 
 class CartDetail(models.Model):
