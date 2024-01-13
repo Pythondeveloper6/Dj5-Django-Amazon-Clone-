4a0c8b6b0fbb1625302948f047bd5523c6f82a3b
Mahmoud Ahmed <pythondeveloper6@gmail.com>
2024-01-13 22:28:19 +0200

    add apply coupon API

--- orders/api.py
+++ orders/api.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+import datetime
 
 from .serializers import CartDetailSerializer,CartSerializer,OrderDetailSerializer,OrderSerializer
 from .models import Order , OrderDetail , Cart , CartDetail , Coupon
@@ -32,3 +34,37 @@ class OrderListAPI(generics.ListAPIView):
 class OrderDetailAPI(generics.RetrieveAPIView):
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
+    
+    
+    
+    
+class ApplyCouponAPI(generics.GenericAPIView):
+    
+    def post(self,request,*args, **kwargs):
+        user = User.objects.get(username=self.kwargs['username'])              # url 
+        coupon = get_object_or_404(Coupon , code=request.data['coupon_code'])   # request body
+        delivery_fee = DeliveryFee.objects.last().fee
+        cart = Cart.objects.get(user=user,status='Inprogress')
+        
+        if coupon and coupon.quantity > 0 :
+            today_date = datetime.datetime.today().date()
+            if today_date >= coupon.start_date and today_date <= coupon.end_date:
+                copoun_value = round(cart.cart_total / 100*coupon.discount,2)
+                sub_total = cart.cart_total - copoun_value
+                
+                cart.coupon = coupon
+                cart.total_with_coupon = sub_total
+                cart.save()
+                
+                coupon.quantity -= 1
+                coupon.save()
+                
+                return Response({'message':'coupn was applied successfully'})
+            
+            else:
+                return Response({'message':'coupoin is invalid or expired'})
+            
+        return Response({'message':'coupon not found'})
+        
+        
+    
--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path 
 from .views import order_list , checkout , add_to_cart
-from .api import OrderListAPI,OrderDetailAPI
+from .api import OrderListAPI,OrderDetailAPI,ApplyCouponAPI
 
 
 
@@ -12,6 +12,7 @@ urlpatterns = [
     
     # api 
     path('api/<str:username>/orders' , OrderListAPI.as_view()),
-    path('api/<str:username>/orders/<int:pk>',OrderDetailAPI.as_view())
+    path('api/<str:username>/orders/<int:pk>',OrderDetailAPI.as_view()),
+    path('api/<str:username>/apply-coupon' , ApplyCouponAPI.as_view()),
     
 ]
