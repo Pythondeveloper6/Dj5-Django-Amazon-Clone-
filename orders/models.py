637b26b47690d37b93b355ecf73b05588edeadf9
Mahmoud Ahmed <pythondeveloper6@gmail.com>
2024-01-13 21:53:53 +0200

    add apply coupon

--- orders/models.py
+++ orders/models.py
@@ -70,7 +70,7 @@ class CartDetail(models.Model):
 class Coupon(models.Model):
     code = models.CharField(max_length=20)
     start_date = models.DateField(default=timezone.now)
-    end_date = models.DateField()
+    end_date = models.DateField(null=True,blank=True)
     quantity = models.IntegerField()
     discount = models.FloatField()
     
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render , redirect
+from django.shortcuts import get_object_or_404
+import datetime
+
 from .models import Order , OrderDetail , Cart , CartDetail , Coupon
 from products.models import Product 
 from settings.models import DeliveryFee
@@ -13,6 +16,35 @@ def checkout(request):
     cart = Cart.objects.get(user=request.user,status='Inprogress')
     cart_detail = CartDetail.objects.filter(cart=cart)
     delivery_fee = DeliveryFee.objects.last().fee
+    
+    
+    if request.method == 'POST':
+        code = request.POST['copoun_code']
+        # coupon = Coupon.objects.get(code=code)
+        coupon = get_object_or_404(Coupon , code=code)
+        
+        if coupon and coupon.quantity > 0 :
+            today_date = datetime.datetime.today().date()
+            if today_date >= coupon.start_date and today_date <= coupon.end_date:
+                copoun_value = round(cart.cart_total / 100*coupon.discount,2)
+                sub_total = cart.cart_total - copoun_value
+                total = sub_total + delivery_fee
+                
+                cart.coupon = coupon
+                cart.total_with_coupon = sub_total
+                cart.save()
+                
+                return render(request,'orders/checkout.html',{
+                    'cart_detail' : cart_detail , 
+                    'delivery_fee': delivery_fee , 
+                    'sub_total': sub_total , 
+                    'discount': copoun_value , 
+                    'total': total
+                })
+                    
+    
+    
+    
     
     sub_total = cart.cart_total
     discount = 0
